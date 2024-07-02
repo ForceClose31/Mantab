a4150833c9bf1ec8804d5471f7537a1d5969fd53
ForceClose31 <forceclose31@gmail.com>
2024-07-02 08:51:10 +0700

    fix: layout colum record domba

--- lib/screens/recordDomba/screen/recordDomba.dart
+++ lib/screens/recordDomba/screen/recordDomba.dart
@@ -51,193 +51,195 @@ class RecordDomba extends StatelessWidget {
                     }
                     var beranakData = beranakSnapshot.data!.docs;
 
-                    return Stack(
-                      children: [
-                        Positioned(
-                          top: 0,
-                          left: 0,
-                          child: Image.asset(
-                            'lib/images/bgbatik.png',
-                            width: 430,
-                            height: 341.5,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Positioned(
-                          top: 25,
-                          left: 20,
-                          right: 20,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Image.asset(
-                                  'lib/images/back.png',
-                                  width: 24,
-                                  height: 24,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          top: 70,
-                          left: 15,
-                          right: 20,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Center(
-                                child: Text(
-                                  'Record Data Domba',
-                                  style: TextStyle(
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          top: 120,
-                          left: 18,
-                          right: 18,
-                          child: Container(
-                            padding: EdgeInsets.all(12),
-                            height: 148,
-                            width: 395,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white,
-                              border: Border.all(color: Colors.black),
+                    return Expanded(
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            child: Image.asset(
+                              'lib/images/bgbatik.png',
+                              width: 430,
+                              height: 341.5,
+                              fit: BoxFit.cover,
                             ),
+                          ),
+                          Positioned(
+                            top: 25,
+                            left: 20,
+                            right: 20,
                             child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                const Text(
-                                  "Rekap Kondisi Domba",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromRGBO(117, 117, 117, 1),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Image.asset(
+                                    'lib/images/back.png',
+                                    width: 24,
+                                    height: 24,
                                   ),
                                 ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Column(
-                                      children: [
-                                        Container(
-                                          alignment: Alignment.center,
-                                          height: 59,
-                                          width: 59,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            color:
-                                                Color.fromARGB(239, 40, 54, 42),
-                                          ),
-                                          child: Text(
-                                            kawinData.length.toString(),
-                                            style: TextStyle(
-                                              fontSize: 26,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(
-                                          "Kawin",
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
-                                            color: Color.fromRGBO(
-                                                117, 117, 117, 1),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    Column(
-                                      children: [
-                                        Container(
-                                          alignment: Alignment.center,
-                                          height: 59,
-                                          width: 59,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            color: Color.fromARGB(
-                                                255, 255, 104, 104),
-                                          ),
-                                          child: Text(
-                                            buntingData.length.toString(),
-                                            style: TextStyle(
-                                              fontSize: 26,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(
-                                          "Bunting",
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
-                                            color: Color.fromRGBO(
-                                                117, 117, 117, 1),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    Column(
-                                      children: [
-                                        Container(
-                                          alignment: Alignment.center,
-                                          height: 59,
-                                          width: 59,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(10),
-                                            color: Color.fromARGB(
-                                                255, 255, 209, 60),
-                                          ),
-                                          child: Text(
-                                            beranakData.length.toString(),
-                                            style: TextStyle(
-                                              fontSize: 26,
-                                              fontWeight: FontWeight.w500,
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        Text(
-                                          "Beranak",
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
-                                            color: Color.fromRGBO(
-                                                117, 117, 117, 1),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ],
-                                )
                               ],
                             ),
                           ),
-                        ),
-                        buttonAddRecord(),
-                        topContent()
-                      ],
+                          Positioned(
+                            top: 70,
+                            left: 15,
+                            right: 20,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Center(
+                                  child: Text(
+                                    'Record Data Domba',
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            top: 120,
+                            left: 18,
+                            right: 18,
+                            child: Container(
+                              padding: EdgeInsets.all(12),
+                              height: 148,
+                              width: 395,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                border: Border.all(color: Colors.black),
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "Rekap Kondisi Domba",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(117, 117, 117, 1),
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          Container(
+                                            alignment: Alignment.center,
+                                            height: 59,
+                                            width: 59,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color:
+                                                  Color.fromARGB(239, 40, 54, 42),
+                                            ),
+                                            child: Text(
+                                              kawinData.length.toString(),
+                                              style: TextStyle(
+                                                fontSize: 26,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
+                                          Text(
+                                            "Kawin",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Color.fromRGBO(
+                                                  117, 117, 117, 1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            alignment: Alignment.center,
+                                            height: 59,
+                                            width: 59,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color: Color.fromARGB(
+                                                  255, 255, 104, 104),
+                                            ),
+                                            child: Text(
+                                              buntingData.length.toString(),
+                                              style: TextStyle(
+                                                fontSize: 26,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
+                                          Text(
+                                            "Bunting",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Color.fromRGBO(
+                                                  117, 117, 117, 1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Column(
+                                        children: [
+                                          Container(
+                                            alignment: Alignment.center,
+                                            height: 59,
+                                            width: 59,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              color: Color.fromARGB(
+                                                  255, 255, 209, 60),
+                                            ),
+                                            child: Text(
+                                              beranakData.length.toString(),
+                                              style: TextStyle(
+                                                fontSize: 26,
+                                                fontWeight: FontWeight.w500,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
+                                          Text(
+                                            "Beranak",
+                                            style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Color.fromRGBO(
+                                                  117, 117, 117, 1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          buttonAddRecord(),
+                          topContent()
+                        ],
+                      ),
                     );
                   },
                 );
